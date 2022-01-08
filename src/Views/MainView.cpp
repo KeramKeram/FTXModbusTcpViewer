f7f62a8ea769bc3a8c684804ff2ae965849a4487
KeramKeram <rev@autograf.pl>
2022-01-08 21:01:30 +0100

    MainView: copy focus example to main view.

--- src/Views/MainView.cpp
+++ src/Views/MainView.cpp
@@ -1,75 +1,75 @@
 #include "MainView.h"
-#include <stdlib.h>  // for EXIT_SUCCESS
-#include <memory>    // for allocator, __shared_ptr_access
+#include <memory>  // for allocator, __shared_ptr_access
+#include <stdlib.h>// for EXIT_SUCCESS
 #include <string>  // for string, operator+, basic_string, to_string, char_traits
 #include <vector>  // for vector, __alloc_traits<>::value_type
 
-#include "ftxui/component/captured_mouse.hpp"  // for ftxui
-#include "ftxui/component/component.hpp"  // for Menu, Renderer, Horizontal, Vertical
-#include "ftxui/component/component_base.hpp"  // for ComponentBase
-#include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
-#include "ftxui/dom/elements.hpp"  // for text, Element, operator|, window, flex, vbox
+#include "ftxui/component/captured_mouse.hpp"    // for ftxui
+#include "ftxui/component/component.hpp"         // for Menu, Renderer, Horizontal, Vertical
+#include "ftxui/component/component_base.hpp"    // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"// for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp"                // for text, Element, operator|, window, flex, vbox
+
+
+#include "ftxui/dom/node.hpp"    // for Render
+#include "ftxui/screen/box.hpp"  // for ftxui
+#include "ftxui/screen/color.hpp"// for Color, Color::Blue, Color::Cyan, Color::White
+
+#include <ftxui/dom/elements.hpp>// for color, Fit, LIGHT, align_right, bold, DOUBLE
+#include <ftxui/dom/table.hpp>   // for Table, TableSelection
 
 namespace views {
     using namespace ftxui;
     void MainView::show() {
-        int menu_selected[] = {0, 0, 0};
-        std::vector<std::vector<std::string>> menu_entries = {
-                {
-                        "Ananas",
-                        "Raspberry",
-                        "Citrus",
-                },
-                {
-                        "Potatoes",
-                        "Weat",
-                        "Rise",
-                },
-                {
-                        "Carrot",
-                        "Lettuce",
-                        "Tomato",
-                },
-        };
+        float focus_x = 0.0f;
+        float focus_y = 0.0f;
 
-        int menu_selected_global = 0;
-        auto menu_global = Container::Vertical(
-                {
-                        Window("Menu 1", Menu(&menu_entries[0], &menu_selected[0])),
-                        Window("Menu 2", Menu(&menu_entries[1], &menu_selected[1])),
-                        Window("Menu 3", Menu(&menu_entries[2], &menu_selected[2])),
-                },
-                &menu_selected_global);
+        auto slider_x = Slider("x", &focus_x, 0.f, 1.f, 0.01f);
+        auto slider_y = Slider("y", &focus_y, 0.f, 1.f, 0.01f);
 
-        auto info = Renderer([&] {
-            int g = menu_selected_global;
-            std::string value = menu_entries[g][menu_selected[g]];
-            return window(text("Content"),  //
-                          vbox({
-                                       text("menu_selected_global = " + std::to_string(g)),
-                                       text("menu_selected[0]     = " +
-                                            std::to_string(menu_selected[0])),
-                                       text("menu_selected[1]     = " +
-                                            std::to_string(menu_selected[1])),
-                                       text("menu_selected[2]     = " +
-                                            std::to_string(menu_selected[2])),
-                                       text("Value                = " + value),
-                               })) |
-                   flex;
-        });
+        auto renderer = Renderer(
+                Container::Vertical({
+                        slider_x,
+                        slider_y,
+                }),
+                [&] {
+                    auto title = "focusPositionRelative(" +      //
+                                 std::to_string(focus_x) + ", " +//
+                                 std::to_string(focus_y) + ")";  //
+                    return vbox({
+                                   text(title),
+                                   separator(),
+                                   slider_x->Render(),
+                                   slider_y->Render(),
+                                   separator(),
+                                   make_grid() | focusPositionRelative(focus_x, focus_y) |
+                                           frame | flex,
+                           }) |
+                           border;
+                });
 
-        auto global = Container::Horizontal({
-                                                    menu_global,
-                                                    info,
-                                            });
-
-        auto screen = ScreenInteractive::TerminalOutput();
-        screen.Loop(global);
+        auto screen = ScreenInteractive::Fullscreen();
+        screen.Loop(renderer);
     }
 
-    Component MainView::Window(std::string title, Component component) {
-        return Renderer(component, [component, title] {  //
-            return window(text(title), component->Render()) | flex;
-        });
-    }
-}
+    Element MainView::make_box(int x, int y) {
+        std::string title = "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+        return text(title) | center | size(WIDTH, EQUAL, 6) |
+               size(HEIGHT, EQUAL, 3) | border |
+               bgcolor(Color::HSV(x * 255 / 15, 255, y * 255 / 15));
+    };
+
+    Element MainView::make_grid() {
+        std::vector<Elements> rows;
+        for (int i = 0; i < 15; i++) {
+            std::vector<Element> cols;
+            for (int j = 0; j < 15; j++) {
+                cols.push_back(make_box(i, j));
+            }
+            rows.push_back(cols);
+        }
+
+        return gridbox(rows);
+    };
+
+}// namespace views
--- src/Views/MainView.h
+++ src/Views/MainView.h
@@ -1,27 +1,28 @@
 #pragma once
 #include "IView.h"
 
-#include <stdlib.h>  // for EXIT_SUCCESS
-#include <memory>    // for allocator, __shared_ptr_access
+#include <memory>  // for allocator, __shared_ptr_access
+#include <stdlib.h>// for EXIT_SUCCESS
 #include <string>  // for string, operator+, basic_string, to_string, char_traits
 #include <vector>  // for vector, __alloc_traits<>::value_type
 
-#include "ftxui/component/captured_mouse.hpp"  // for ftxui
-#include "ftxui/component/component.hpp"  // for Menu, Renderer, Horizontal, Vertical
-#include "ftxui/component/component_base.hpp"  // for ComponentBase
-#include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
-#include "ftxui/dom/elements.hpp"  // for text, Element, operator|, window, flex, vbox
+#include "ftxui/component/captured_mouse.hpp"    // for ftxui
+#include "ftxui/component/component.hpp"         // for Menu, Renderer, Horizontal, Vertical
+#include "ftxui/component/component_base.hpp"    // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"// for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp"                // for text, Element, operator|, window, flex, vbox
 
 namespace views {
     using namespace ftxui;
 
-    class MainView: public IView {
+    class MainView : public IView {
     public:
         ~MainView() override = default;
 
         void show() override;
 
     private:
-        Component Window(std::string title, Component component);
+        Element make_box(int x, int y);
+        Element make_grid();
     };
-}
+}// namespace views
