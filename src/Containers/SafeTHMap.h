b881663c738d80ce7181d7faa5474148d5450139
KeramKeram <rev@autograf.pl>
2022-01-09 20:36:08 +0100

    Containers: Implement add method for safe thread map.

--- src/Containers/SafeTHMap.h
+++ src/Containers/SafeTHMap.h
@@ -1,7 +1,28 @@
-
 #pragma once
 
+#include <map>
+#include <mutex>
+
 namespace containers {
+    template<class T, class V>
     class SafeTHMap {
+    public:
+        SafeTHMap() = default;
+        ~SafeTHMap() = default;
+
+        void add(T &key, V &value) {
+            std::lock_guard<std::mutex> lk(mMutex);
+            mContainer[key] = value;
+        }
+
+        void remove(T &key) {
+        }
+
+        V get(T &key) {
+        }
+
+    private:
+        std::map<T, V> mContainer;
+        std::mutex mMutex;
     };
-}
+}// namespace containers
