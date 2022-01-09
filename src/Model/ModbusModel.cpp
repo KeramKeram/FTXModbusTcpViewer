725c9f9b1a33712a952f91ec3ccd6a6d0f42e7a7
KeramKeram <rev@autograf.pl>
2022-01-09 20:51:29 +0100

    Model: Create modbus model.
    This is struct with thread safe containers based on map. We have four containers each for every part of modbus memory.

--- src/Model/ModbusModel.cpp
+++ src/Model/ModbusModel.cpp
@@ -1,2 +1,10 @@
 #include "ModbusModel.h"
- namespace model {}
+namespace model {
+
+    ModbusModel::ModbusModel() {
+        mCoils = std::make_unique<safeMap>();
+        mInputStatus = std::make_unique<safeMap>();
+        mInputRegisters = std::make_unique<safeMap>();
+        mHoldingRegisters = std::make_unique<safeMap>();
+    }
+}// namespace model
--- src/Model/ModbusModel.h
+++ src/Model/ModbusModel.h
@@ -1,6 +1,17 @@
 #pragma once
+#include <memory>
 
-namespace moedl {
-    class ModbusModel {
+#include "SafeTHMap.h"
+
+namespace model {
+    using safeMap = containers::SafeTHMap<std::string, std::string>;
+    struct ModbusModel {
+        ModbusModel();
+        ~ModbusModel() = default;
+
+        std::unique_ptr<safeMap> mCoils;
+        std::unique_ptr<safeMap> mInputStatus;
+        std::unique_ptr<safeMap> mInputRegisters;
+        std::unique_ptr<safeMap> mHoldingRegisters;
     };
-}
+}// namespace model
