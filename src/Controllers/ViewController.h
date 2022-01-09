158c6fb28a58f8bf2e315b7aa88afa5581b36298
KeramKeram <rev@autograf.pl>
2022-01-09 20:58:45 +0100

    Controller: Add to view controller modbus model and view.

--- src/Controllers/ViewController.h
+++ src/Controllers/ViewController.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <memory>
+
+#include "MainView.h"
+#include "ModbusModel.h"
+
 namespace controllers {
     class ViewController {
+    private:
+        model::ModbusModel mModbusModel;
+        std::unique_ptr<views::IView> mMainView;
     };
 }
