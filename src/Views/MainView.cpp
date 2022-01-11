776b8dcbefb4863b89e8d0f5e2df5ac446f4d58b
KeramKeram <rev@autograf.pl>
2022-01-11 22:29:34 +0100

    MainView: Clang-tidy analysis. Remove unused includes.

--- src/Views/MainView.cpp
+++ src/Views/MainView.cpp
@@ -1,22 +1,13 @@
 #include "MainView.h"
-#include <memory>  // for allocator, __shared_ptr_access
-#include <stdlib.h>// for EXIT_SUCCESS
 #include <string>  // for string, operator+, basic_string, to_string, char_traits
 #include <vector>  // for vector, __alloc_traits<>::value_type
 
-#include "ftxui/component/captured_mouse.hpp"    // for ftxui
 #include "ftxui/component/component.hpp"         // for Menu, Renderer, Horizontal, Vertical
-#include "ftxui/component/component_base.hpp"    // for ComponentBase
 #include "ftxui/component/screen_interactive.hpp"// for Component, ScreenInteractive
 #include "ftxui/dom/elements.hpp"                // for text, Element, operator|, window, flex, vbox
 
 
-#include "ftxui/dom/node.hpp"    // for Render
-#include "ftxui/screen/box.hpp"  // for ftxui
 #include "ftxui/screen/color.hpp"// for Color, Color::Blue, Color::Cyan, Color::White
-
-#include <ftxui/dom/elements.hpp>// for color, Fit, LIGHT, align_right, bold, DOUBLE
-#include <ftxui/dom/table.hpp>   // for Table, TableSelection
 
 namespace views {
     using namespace ftxui;
