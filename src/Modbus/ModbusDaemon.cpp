fca8998f8c5129a28e0d0d2eb7c7dca0be2e0e5f
KeramKeram <rev@autograf.pl>
2022-01-23 15:05:37 +0100

    ModbusDaemon: Implement basic run, stop function for thread.

--- src/Modbus/ModbusDaemon.cpp
+++ src/Modbus/ModbusDaemon.cpp
@@ -1,3 +1,23 @@
 #include "ModbusDaemon.h"
 
-namespace modbus {}
+namespace modbus {
+
+    ModbusDaemon::ModbusDaemon(const std::shared_ptr<model::ModbusModel> &mModbusModel) : mModbusModel(mModbusModel) {}
+
+    void ModbusDaemon::run() {
+        mThread = std::thread(&ModbusDaemon::runFunction, this);
+    }
+    void ModbusDaemon::stopThread() {
+        mRun.store(false);
+        if (mThread.joinable()){
+            mThread.join();
+        }
+    }
+
+    void ModbusDaemon::runFunction() {
+        mRun.store(true);
+        while (mRun.load()) {
+
+        }
+    }
+}
--- src/Modbus/ModbusDaemon.h
+++ src/Modbus/ModbusDaemon.h
@@ -1,5 +1,33 @@
 #pragma once
+#include <memory>
+#include <thread>
+#include <atomic>
+#include <queue>
+#include <random>
+#include <algorithm>
+#include <iostream>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+#include "ModbusModel.h"
 
 namespace modbus {
-    class ModbusDaemon {};
+    class ModbusDaemon {
+    public:
+        ModbusDaemon(const std::shared_ptr<model::ModbusModel> &mModbusModel);
+
+        void run();
+
+        void stopThread();
+
+    private:
+
+        void runFunction();
+
+        std::shared_ptr<model::ModbusModel> mModbusModel;
+        std::thread mThread;
+        std::atomic_bool mRun;
+        std::mutex mMutex;
+    };
 }// namespace modbus
