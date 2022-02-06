0a9d2b32973520b435e228c8d76bb997ec232140
KeramKeram <rev@autograf.pl>
2022-02-06 20:21:16 +0100

    MainView: Remove not needed constexpr.

--- src/Views/MainView.h
+++ src/Views/MainView.h
@@ -17,11 +17,6 @@
 namespace views {
 using namespace ftxui;
 
-/*constexpr int VIEW_COLUMNS    = 2;
-constexpr int VIEW_ROWS       = 2;
-constexpr int VIEW_BOX_WIDTH  = 8;
-constexpr int VIEW_BOX_HEIGHT = 3;*/
-
 class MainView : public IView
 {
 public:
